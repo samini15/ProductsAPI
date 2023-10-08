f41a04ec4c4d07e62afc40a5504917d0d1341137
Shayan <shayan.amini76@gmail.com>
2023-10-08 18:46:40 +0200

    Add product detail screen

---
+++ ProductsAPI/View/Custom/TextWithRoundedRectangleOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  TextWithRoundedRectangleOverlay.swift
+//  ProductsAPI
+//
+//  Created by Shayan Amini on 08/10/2023.
+//
+
+import SwiftUI
+
+struct TextWithRoundedRectangleOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextWithRoundedRectangleOverlay()
+}
