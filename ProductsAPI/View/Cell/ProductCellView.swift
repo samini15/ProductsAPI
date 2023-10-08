8d537a5365a7869f396f1b8199e0f9e6c031fa9e
Shayan <shayan.amini76@gmail.com>
2023-10-08 18:15:20 +0200

    Display products + Design of product cell

---
+++ ProductsAPI/View/Cell/ProductCellView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCellView.swift
+//  ProductsAPI
+//
+//  Created by Shayan Amini on 08/10/2023.
+//
+
+import SwiftUI
+
+struct ProductCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductCellView()
+}
---
+++ ProductsAPI/View/ProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailView.swift
+//  ProductsAPI
+//
+//  Created by Shayan Amini on 08/10/2023.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}
