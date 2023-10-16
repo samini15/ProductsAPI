2a06dc46220b4eca08216692b5a001fb21de7169
Shayan <shayan.amini76@gmail.com>
2023-10-16 12:10:43 +0200

    Init pods + Add Kingfisher pod to support async image caching

---
+++ ProductsAPI/View/Custom/AsyncImageView.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncImageView.swift
+//  ProductsAPI
+//
+//  Created by Shayan Amini on 12/10/2023.
+//
+
+import SwiftUI
+
+struct AsyncImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncImageView()
+}
---
+++ ProductsAPI/View/Custom/CacheAsyncImage.swift
@@ -0,0 +1,18 @@
+//
+//  CacheAsyncImage.swift
+//  ProductsAPI
+//
+//  Created by Shayan Amini on 12/10/2023.
+//
+
+import SwiftUI
+
+struct CacheAsyncImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CacheAsyncImage()
+}
